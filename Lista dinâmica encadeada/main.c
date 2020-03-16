434938f79bf1101e4bffa40189b3997b744d10f6
Tungsinn <gvnalamas@tutanota.com>
2020-03-16 12:20:35 -0300

    Novos testes na main...

--- Lista dinâmica encadeada/main.c
+++ Lista dinâmica encadeada/main.c
@@ -6,9 +6,11 @@
 #include "funcoes.c"
 
 int main() {
-    lista L;
+    lista L; 
+    lista L2;
     int elemento;
     int i = 0;
+	int verifica;
     
     srand((unsigned) time(NULL));
     elemento = randomNum();
@@ -19,7 +21,20 @@ int main() {
         elemento = randomNum();
         insereOrdenado(&L, elemento);
     }
+	if(verifica = verificaOrdenacao(&L, "crescente"))
+		printf("\nA lista está em ordem crescente!");
+	else printf("\nA lista não está em ordem crescente...");
+	
+	if(!(verifica = verificaOrdenacao(&L, "descrescente")))
+	    printf("\nA lista está em ordem descrescente!");
+	else printf("\nA lista não está em ordem decrescente...");
+	
     printf("\n\nIMPRESSÃO:");
     imprime(&L);
+	
+	inicializaLista(&L2);
+	copiaLista(&L, &L2);
+	printf("\n\nIMPRESSÃO L2:");
+	imprime(&L2);
     return 0;
 }
